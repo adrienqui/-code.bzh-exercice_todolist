be2c21e965b841ef275b30da52dd0d8c8ea12ae4
Adrien Quillivic <adrienqui@gmail.com>
2019-06-03 20:24:21 +0200

    Etape 3

--- todolist.js
+++ todolist.js
@@ -1,8 +1,6 @@
 var app = new Vue({
   el: '#app',
   data: {
-    nom: '',
-    error: false,
     taches: [
       { id: 0, nom: 'Course', done: false },
       { id: 1, nom: 'Poste', done: true },
@@ -10,19 +8,6 @@ var app = new Vue({
     ]
   },
   methods: {
-    add: function() {
-      if (this.nom) {
-        this.error = false;
-        this.taches.push({
-          id: (this.taches.length) ? this.taches[this.taches.length - 1].id + 1 : 0,
-          nom: this.nom,
-          done: false
-        });
-        this.nom = ''
-      } else {
-        this.error = true
-      }
-    },
     remove: function(index) {
       this.taches.splice(index, 1)
     }
